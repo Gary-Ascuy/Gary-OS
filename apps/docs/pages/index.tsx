b21debadf87006a8de68efc0ca4fe7d82fa4cc38
Gary Ascuy <gary.ascuy@gmail.com>
2022-06-04 13:58:33 -0500

    Improving format and settings for prettier

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@garyos/window";
+import { Button } from '@garyos/window'
 
 export default function Docs() {
   return (
@@ -6,5 +6,5 @@ export default function Docs() {
       <h1>Docs</h1>
       <Button />
     </div>
-  );
+  )
 }
