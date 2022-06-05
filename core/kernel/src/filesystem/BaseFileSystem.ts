028282c532455cad0cbae50fbae54f6fd8c564f2
Gary Ascuy <gary.ascuy@gmail.com>
2022-06-04 22:46:56 -0500

    Adding filesystem files

---
+++ core/kernel/src/filesystem/BaseFileSystem.ts
@@ -0,0 +1,23 @@
+import { readFile, writeFile } from './io'
+import { VirtualFile } from './VirtualFile'
+import { FileStream, VirtualFileSystem } from './VirtualFileSystem'
+
+export abstract class BaseFileSystem implements VirtualFileSystem {
+  abstract mount(): Promise<void>
+  abstract unmount(): Promise<void>
+
+  abstract getFile(path: string, exclusive: boolean): Promise<VirtualFile>
+  abstract free(file: VirtualFile | string): Promise<void>
+
+  abstract open(path: string, mode: string): Promise<FileStream>
+  abstract exist(path: string): Promise<boolean>
+  abstract remove(path: string): Promise<void>
+
+  readFile(path: string): Promise<string> {
+    return readFile(this, path)
+  }
+
+  writeFile(path: string, content: string, mode: string): Promise<void> {
+    return writeFile(this, path, content, mode)
+  }
+}
