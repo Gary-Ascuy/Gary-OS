8edd5b3191206d8c8765331128dbaee9f4ed9a7f
Gary Ascuy <gary.ascuy@gmail.com>
2022-07-05 19:26:18 -0500

    Adding resizesible support

--- apps/web/components/Desktop/Window/Window.tsx
+++ apps/web/components/Desktop/Window/Window.tsx
@@ -5,6 +5,7 @@ import WindowHeader from './WindowHeader'
 import { WindowOption, Box } from './WindowOption'
 
 import style from './Window.module.css'
+import { Resizable } from 're-resizable'
 
 export interface WindowProps extends WindowOption {
   children?: JSX.Element | JSX.Element[]
@@ -19,14 +20,16 @@ export default function WindowProps({ title, box, children }: WindowProps): JSX.
   const { x, y, width, height } = useMemo(() => box || getDefafaultBox(), [box])
 
   return (
-    <Draggable handle="section.title-dragable">
-      <section style={{ left: y, top: y, width, height }} className={style.window}>
-        <WindowHeader title={title} />
+    <Draggable handle="section.title-dragable" defaultPosition={{ x, y }}>
+      <Resizable defaultSize={{ width, height }} >
+        <section style={{ left: 0, top: 0, width: '100%', height: '100%' }} className={style.window}>
+          <WindowHeader title={title} />
 
-        <section className={style.body}>
-          {children}
+          <section className={style.body}>
+            {children}
+          </section>
         </section>
-      </section>
+      </Resizable>
     </Draggable>
   )
 }
